b4845c9e93aa69a2a51ac93210a26999f210d783
FindAny <neelammsr@gmail.com>
2024-08-15 10:51:18 +0530

    dress shop added

--- lib/shopping/dresshome.dart
+++ lib/shopping/dresshome.dart
@@ -4,7 +4,7 @@ import 'package:findany_flutter/shopping/cartpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
+import 'package:shimmer/shimmer.dart';
 import 'dressdetailspage.dart';
 import 'dressuploadpage.dart';
 
@@ -17,16 +17,27 @@ class _DressHomeState extends State<DressHome> {
   bool _isOwner = false;
   FireStoreService fireStoreService = FireStoreService();
   String category = "Men";
-  late Stream<QuerySnapshot> _dressStream;
+  String? selectedSubcategory;
+  List<DocumentSnapshot> products = [];
+  DocumentSnapshot? lastDocument;
+  bool isLoading = false;
+  bool isLoadingMore = false;
+  bool hasMoreProducts = true;
   String _searchQuery = "";
   bool _isSearching = false;
   final TextEditingController _searchController = TextEditingController();
+
+  Map<String, List<String>> subcategories = {
+    'Men': ['Shirts', 'Short', 'Shoes', 'T-shirt'],
+    'Women': ['Dresses', 'Tops', 'Shoes'],
+    'Kids': ['T-Shirts', 'Pants', 'Shoes'],
+  };
 
   @override
   void initState() {
     super.initState();
     _checkIfOwner();
-    _dressStream = _getDressStream();
+    _fetchInitialProducts();
   }
 
   void _checkIfOwner() async {
@@ -39,20 +50,73 @@ class _DressHomeState extends State<DressHome> {
   Future<bool> isUserOwner() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      DocumentReference documentReference = FirebaseFirestore.instance.doc("/AdminDetails/DressShop");
-      Map<String, dynamic>? data = await fireStoreService.getDocumentDetails(documentReference);
+      DocumentReference documentReference =
+      FirebaseFirestore.instance.doc("/AdminDetails/DressShop");
+      Map<String, dynamic>? data =
+      await fireStoreService.getDocumentDetails(documentReference);
       Iterable ownerDetails = data!.values;
       return ownerDetails.contains(user.email);
     }
     return false;
   }
 
-  Stream<QuerySnapshot> _getDressStream() {
-    return FirebaseFirestore.instance
-        .collection('/SHOPS/DRESSSHOP/$category')
-        .where('name', isGreaterThanOrEqualTo: _searchQuery)
-        .where('name', isLessThan: _searchQuery + 'z')
-        .snapshots();
+  Future<void> _fetchInitialProducts() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    Query query = _buildQuery();
+
+    QuerySnapshot querySnapshot = await query.limit(4).get();
+    setState(() {
+      products = querySnapshot.docs;
+      if (products.isNotEmpty) {
+        lastDocument = products.last;
+      }
+      isLoading = false;
+      hasMoreProducts = querySnapshot.docs.length == 4;
+    });
+  }
+
+  Future<void> _fetchMoreProducts() async {
+    if (isLoadingMore || !hasMoreProducts) return;
+
+    setState(() {
+      isLoadingMore = true;
+    });
+
+    Query query = _buildQuery().startAfterDocument(lastDocument!);
+
+    QuerySnapshot querySnapshot = await query.limit(4).get();
+    setState(() {
+      products.addAll(querySnapshot.docs);
+      if (querySnapshot.docs.isNotEmpty) {
+        lastDocument = querySnapshot.docs.last;
+      }
+      isLoadingMore = false;
+      hasMoreProducts = querySnapshot.docs.length == 4;
+    });
+  }
+
+  Query _buildQuery() {
+    CollectionReference collectionRef = FirebaseFirestore.instance
+        .collection('/SHOPS/DRESSSHOP/$category');
+
+    Query query = collectionRef;
+
+    // Apply subcategory filter if selected
+    if (selectedSubcategory != null) {
+      query = query.where('subCategory', isEqualTo: selectedSubcategory);
+    }
+
+    // Apply search query filter
+    if (_searchQuery.isNotEmpty) {
+      query = query
+          .where('name', isGreaterThanOrEqualTo: _searchQuery)
+          .where('name', isLessThan: _searchQuery + 'z');
+    }
+
+    return query;
   }
 
   Future<void> _deleteProduct(DocumentReference productRef) async {
@@ -74,7 +138,8 @@ class _DressHomeState extends State<DressHome> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Delete Product"),
-          content: Text("Are you sure you want to delete this product permanently?"),
+          content: Text(
+              "Are you sure you want to delete this product permanently?"),
           actions: [
             TextButton(
               child: Text("Cancel"),
@@ -107,7 +172,7 @@ class _DressHomeState extends State<DressHome> {
           onChanged: (query) {
             setState(() {
               _searchQuery = query;
-              _dressStream = _getDressStream(); // Update the stream when search query changes
+              _fetchInitialProducts(); // Update the products when search query changes
             });
           },
           decoration: InputDecoration(
@@ -125,7 +190,7 @@ class _DressHomeState extends State<DressHome> {
                   _isSearching = false;
                   _searchController.clear();
                   _searchQuery = "";
-                  _dressStream = _getDressStream(); // Reset the stream
+                  _fetchInitialProducts(); // Reset the products
                 });
               },
             )
@@ -171,34 +236,239 @@ class _DressHomeState extends State<DressHome> {
               ),
             ),
           ),
+          if (subcategories.containsKey(category))
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Container(
+                height: 50.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: subcategories[category]!.map((subcategory) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedSubcategory = subcategory;
+                          _fetchInitialProducts(); // Update the products when subcategory changes
+                        });
+                      },
+                      child: Container(
+                        width: 100.0,
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        decoration: BoxDecoration(
+                          color: selectedSubcategory == subcategory
+                              ? Colors.orange
+                              : Colors.grey,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            subcategory,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
           Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: _dressStream,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
+            child: isLoading
+                ? _buildShimmerLoader()
+                : products.isEmpty
+                ? Center(child: Text('No dresses available'))
+                : NotificationListener<ScrollNotification>(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollEndNotification &&
+                    scrollNotification.metrics.pixels ==
+                        scrollNotification.metrics.maxScrollExtent) {
+                  _fetchMoreProducts();
                 }
-                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                  return Center(child: Text('No products available'));
-                }
-                return GridView.builder(
-                  padding: EdgeInsets.all(8.0),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                    childAspectRatio: 2 / 3,
-                  ),
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot doc = snapshot.data!.docs[index];
-                    return _buildProductItem(doc);
-                  },
-                );
+                return false;
               },
+              child: GridView.builder(
+                padding: EdgeInsets.all(8.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 2 / 3,
+                ),
+                itemCount: products.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == products.length) {
+                    return isLoadingMore
+                        ? Center(child: CircularProgressIndicator())
+                        : SizedBox(); // Empty space at the bottom if not loading more
+                  }
+                  DocumentSnapshot doc = products[index];
+                  return _buildProductItem(doc);
+                },
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildShimmerLoader() {
+    return GridView.builder(
+      padding: EdgeInsets.all(8.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 2 / 3,
+      ),
+      itemCount: 4, // Number of shimmer placeholders to display
+      itemBuilder: (context, index) {
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.white,
+                  height: 16.0,
+                  width: 80.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  color: Colors.white,
+                  height: 16.0,
+                  width: 40.0,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCategoryItem(String categoryName) {
+    bool isSelected = categoryName == category;
+
+    // Map of category images
+    Map<String, String> categoryImages = {
+      'Men': 'assets/images/mendress.png',
+      'Women': 'assets/images/womendress.png',
+      'Kids': 'assets/images/kidsdress.png',
+    };
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          category = categoryName;
+          selectedSubcategory = null;
+          _fetchInitialProducts(); // Update the products when category changes
+        });
+      },
+      child: Container(
+        width: 100.0,
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blue : Colors.grey,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display the category image and fit it to the screen
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0), // Match the container's border radius
+              child: Image.asset(
+                categoryImages[categoryName]!,
+                fit: BoxFit.contain, // Ensures the whole image is visible within the container
+                width: 60.0, // Specify width to ensure uniformity
+                height: 60.0, // Specify height to ensure uniformity
+              ),
+            ),
+            SizedBox(height: 8.0),
+            // Display the category name
+            Text(
+              categoryName,
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _buildProductItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    String name = data['name'] ?? 'No Name';
+    String price = data['price']?.toString() ?? 'No Price';
+    String? mediaUrl = data['media']?[0]; // Get the first image URL
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductDetailPage(documentSnapshot: document),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: CachedNetworkImage(
+                imageUrl: mediaUrl ?? '',
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) =>
+                    Center(child: Icon(Icons.error)),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text('Rs. $price'),
+            ),
+            if (_isOwner)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _confirmDeleteProduct(document.reference),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
@@ -208,98 +478,6 @@ class _DressHomeState extends State<DressHome> {
       context,
       MaterialPageRoute(
         builder: (context) => MerchantUploadPage(),
-      ),
-    );
-  }
-
-  Widget _buildCategoryItem(String title) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          category = title;
-          _dressStream = _getDressStream(); // Update the stream when category changes
-        });
-      },
-      child: Container(
-        width: 80.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 30.0,
-              backgroundColor: Colors.blue,
-              child: Icon(Icons.category, color: Colors.white),
-            ),
-            SizedBox(height: 8.0),
-            Text(title),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProductItem(DocumentSnapshot doc) {
-    String imageUrl = (doc['media'] as List).isNotEmpty ? doc['media'][0] : 'https://via.placeholder.com/150';
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ProductDetailPage(documentSnapshot: doc),
-          ),
-        );
-      },
-      child: Card(
-        child: Stack(
-          children: [
-            // Product Image
-            CachedNetworkImage(
-              imageUrl: imageUrl,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => Center(
-                child: CircularProgressIndicator(),
-              ),
-              errorWidget: (context, url, error) => Image.asset('assets/images/shop.png', fit: BoxFit.cover),
-              height: 200.0, // Adjust the height as needed
-              width: double.infinity, // Fill the width of the container
-            ),
-            // Delete Button
-            if (_isOwner)
-              Positioned(
-                top: 8.0,
-                right: 8.0,
-                child: IconButton(
-                  icon: Icon(Icons.delete, color: Colors.red, size: 24.0),
-                  onPressed: () {
-                    _confirmDeleteProduct(doc.reference);
-                  },
-                ),
-              ),
-            // Product Details
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                color: Colors.white.withOpacity(0.7),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      doc['name'] ?? 'Product Name',
-                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '\₹${(doc['price'] ?? 0).toInt()}',
-                      style: TextStyle(color: Colors.green, fontSize: 16.0),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
